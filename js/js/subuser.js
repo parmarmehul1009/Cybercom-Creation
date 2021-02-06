3e85dc1165559b2519207d0c671b98154b099536
parmarmehul1009 <parmarmehul10091999@gmail.com>
2021-02-06 17:16:26 +0530

    final commit 2

--- js/js/subuser.js
+++ js/js/subuser.js
@@ -13,6 +13,27 @@ if (!sessionStorage.getItem('user')) {
     window.location.href = "login.html";
 }
 
+
+var wish_birthday = document.getElementById('wish_birthday');
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+today = mm + '-' + dd;
+let dob = users['birthdate'];
+let date1 = dob.split('-');
+let mm1 = date1[1];
+let dd1 = date1[0];
+let birthdate = mm1 + '-' + dd1;
+// alert(dob);
+if (today == birthdate) {
+    wish_birthday.textContent = 'Happy Birthday!';
+};
+
+
+
+
+
 logout.addEventListener('click', function() {
     sessionStorage.removeItem('user');
     let userArr2 = JSON.parse(localStorage.getItem('user'));
