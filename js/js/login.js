9587ce729e055453f0a43cc389a53dd5fb114b04
parmarmehul1009 <parmarmehul10091999@gmail.com>
2021-02-06 12:55:07 +0530

    2 hour commit

--- js/js/login.js
+++ js/js/login.js
@@ -1,1 +1,30 @@
-// This is login js file
+var register = document.getElementById('register');
+var login = document.getElementById('login');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const errormessage = document.getElementById('errormessage');
+let admin = localStorage.getItem('admin');
+if (localStorage.getItem('admin')) {
+    register.setAttribute('disabled', 'disabled');
+    // register.style.opacity = '.5';
+}
+login.addEventListener('click', function() {
+    if (admin) {
+        let admin = JSON.parse(localStorage.getItem('admin'));
+
+        emailValue = email.value;
+        passwordValue = password.value;
+
+
+        for (let adm in admin) {
+
+            if (emailValue === admin[adm]['email'] && passwordValue === admin[adm]['password']) {
+                alert('Welcome Admin.!');
+                sessionStorage.setItem("admin", JSON.stringify(admin[adm]));
+                window.location = 'dashboard.html';
+            }
+        }
+    } else {
+        alert('No Admin Register Here');
+    }
+});
--- js/js/registration.js
+++ js/js/registration.js
@@ -1,1 +1,45 @@
 // this is registration page javascript
+
+var admin = [];
+if (localStorage.getItem('admin')) {
+    alert('admin already registed');
+    window.location.href = "login.html";
+}
+
+
+
+function isChecked(checkbox, sub1) {
+    document.getElementById(sub1).disabled = !checkbox.checked;
+}
+
+function addadmin() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var cpassword = document.getElementById('con_password').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+
+    if (password !== cpassword) {
+        alert('Password And Confirm Password Dose not match');
+        return false;
+    }
+
+    admindetails = {
+        name: name,
+        email: email,
+        password: password,
+        cpassword: cpassword,
+        city: city,
+        state: state,
+    }
+
+    if (localStorage.getItem('admin')) {
+        alert('admin already registed');
+    } else {
+        admin.push(admindetails);
+        localStorage.setItem('admin', JSON.stringify(admin));
+        alert('Registration Success!');
+        window.location.href = "login.html";
+    }
+};
